b7ee88de6d56c95ca95b13c75970a50605b0b630
Yuichi Motoyama <yomichi@tsg.jp>
2013-07-30 10:33:48 +0900

    rename

--- sample/rw_minimum.jl
+++ sample/rw_minimum.jl
@@ -1,10 +1,7 @@
-N = 1024
-T = 1024
+N = length(ARGS)>0 ? int(ARGS[1]) : 1024
+T = length(ARGS)>1 ? int(ARGS[2]) : 1024
 xs = zeros(Float64,N)
 for t in 0:T
   println("$t $(mean(xs)) $(var(xs))")
   map!(x->rand()<0.5?x+1:x-1,xs)
-  map!(xs) do x
-    rand() < 0.5 ? x+1 : x-1
-  end
 end
